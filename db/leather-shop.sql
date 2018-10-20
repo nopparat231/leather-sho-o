2f31976eeda015700e126e66440b0a235e3e1e72
nopparat231 <23.noop@gmail.com>
2018-10-20 18:11:42 +0700

    up sql

--- db/leather-shop.sql
+++ db/leather-shop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 18, 2018 at 05:57 PM
+-- Generation Time: Oct 20, 2018 at 01:10 PM
 -- Server version: 10.1.32-MariaDB
 -- PHP Version: 5.6.36
 
@@ -34,7 +34,7 @@ CREATE TABLE `tbl_admin` (
   `admin_pass` varchar(20) NOT NULL,
   `admin_name` varchar(100) NOT NULL,
   `admin_address` varchar(100) NOT NULL,
-  `admin_tel` int(10) NOT NULL,
+  `admin_tel` varchar(10) NOT NULL,
   `admin_email` varchar(50) NOT NULL,
   `status` varchar(10) NOT NULL,
   `date_save` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
@@ -45,8 +45,9 @@ CREATE TABLE `tbl_admin` (
 --
 
 INSERT INTO `tbl_admin` (`admin_id`, `admin_user`, `admin_pass`, `admin_name`, `admin_address`, `admin_tel`, `admin_email`, `status`, `date_save`) VALUES
-(1, '1', '1', 'admin', 'test add', 1234567890, 'e-mail@admin.com', 'admin', '2017-08-30 01:57:41'),
-(2, 'staff', '2', 'staff', 'test staff', 123456789, 'e-mail@staff.com', 'staff', '2018-01-25 04:13:06');
+(1, '1', '1', 'admin', 'test add', '0123456789', 'e-mail@admin.com', 'admin', '2017-08-30 01:57:41'),
+(2, 'staff', '2', 'staff', 'test staff', '0123456789', 'e-mail@staff.com', 'staff', '2018-01-25 04:13:06'),
+(10, 'superadmin', 'superadmin', 'superadmin', 'superadmin add', '0222222222', '23superadmin@superadmin.com', 'superadmin', '2018-10-20 06:16:32');
 
 -- --------------------------------------------------------
 
@@ -72,6 +73,18 @@ INSERT INTO `tbl_bank` (`b_id`, `b_number`, `b_name`, `b_type`, `b_owner`, `bn_n
 (2, '365-3032132-3212', 'กรุงไทย', 'ออมทรัพ', 'สมชาย ใจดี', 'พระสมุทรเจดีย์', 'imgb87159464020180819_000254.jpg'),
 (3, '543-1534-54634-212', 'กสิกรไทย', 'ออมทรัพย์', 'มงคล ศรีสุข', 'พระสมุทรเจดีย์', 'imgb197504342620180818_223441.png'),
 (4, '445-354-3232-212', 'ไทยพาณิชย์', 'ออมทรัพย์', 'สมบูรณ์ มั่งมี', 'พระสมุทรเจดีย์', 'imgb133828463220180818_223607.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_log`
+--
+
+CREATE TABLE `tbl_log` (
+  `log_id` int(10) NOT NULL,
+  `log_user` varchar(50) NOT NULL,
+  `log_login` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -106,16 +119,6 @@ INSERT INTO `tbl_member` (`mem_id`, `mem_name`, `mem_address`, `mem_tel`, `mem_u
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tbl_new`
---
-
-CREATE TABLE `tbl_new` (
-  `new_id` int(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `tbl_order`
 --
 
@@ -141,9 +144,9 @@ CREATE TABLE `tbl_order` (
 --
 
 INSERT INTO `tbl_order` (`order_id`, `mem_id`, `name`, `address`, `email`, `phone`, `order_status`, `pay_slip`, `b_name`, `b_number`, `pay_date`, `pay_amount`, `postcode`, `order_date`) VALUES
-(000002, 17, 'ddd', 'ddd', '23.noop@gmail.com', '2147483647', 4, '184612349120180922_150512.jpg', 'กสิกรไทย', '543-1534-54634-212', '2018-09-22', 217220.00, '', '2018-09-22 15:02:43'),
-(000003, 17, 'ddd', 'ddd', '23.noop@gmail.com', '2147483647', 4, '', '', '', '0000-00-00', 0.00, '', '2018-10-07 15:02:13'),
-(000004, 17, 'ddd', 'ddd', '23.noop@gmail.com', '2147483647', 1, '', '', '', '0000-00-00', 0.00, '', '2018-10-18 22:42:54');
+(000006, 17, 'ddd', 'ddd', '23.noop@gmail.com', '3333333333', 2, '50476915920181020_143424.jpg', 'กสิกรไทย', '543-1534-54634-212', '2018-10-20', 9014.30, '', '2018-10-20 14:33:58'),
+(000007, 17, 'ddd', 'ddd', '23.noop@gmail.com', '3333333333', 4, '', '', '', '0000-00-00', 0.00, '', '2018-10-20 14:52:58'),
+(000008, 17, 'ddd', 'ddd', '23.noop@gmail.com', '3333333333', 1, '', '', '', '0000-00-00', 0.00, '', '2018-10-20 15:14:15');
 
 -- --------------------------------------------------------
 
@@ -165,12 +168,10 @@ CREATE TABLE `tbl_order_detail` (
 --
 
 INSERT INTO `tbl_order_detail` (`d_id`, `order_id`, `p_id`, `p_name`, `p_c_qty`, `total`) VALUES
-(3, 2, 45, 'กระเป๋าใส่พวงกุญแจหนัง Gaston Blue', 3, 5248.35),
-(4, 2, 43, 'หมวกกันน็อคหนัง Biker Z1 clear', 12, 204734.00),
-(5, 2, 33, 'กระเป๋าหนังสะพายข้าง สี Oil Chocolate', 1, 3247.45),
-(6, 3, 46, 'สายกระเป๋าหนัง Strap', 1, 529.65),
-(7, 3, 45, 'กระเป๋าใส่พวงกุญแจหนัง Gaston Blue', 5, 8747.25),
-(8, 4, 45, 'กระเป๋าใส่พวงกุญแจหนัง Gaston Blue', 1, 1782.15);
+(11, 6, 44, 'หมวกหนังวัวแท้ Quinn Hat Coffee ', 1, 2981.15),
+(12, 6, 40, 'แจ็คเก็ตหนังPU สำหรับ Biker', 1, 6033.15),
+(13, 7, 47, 'รองเท้าหนังแท้ Chilling Loafer Brown', 1, 4681.55),
+(14, 8, 37, 'กระเป๋าสตางค์ Wallet Easy-S Black', 1, 1891.15);
 
 -- --------------------------------------------------------
 
@@ -203,15 +204,15 @@ CREATE TABLE `tbl_product` (
 --
 
 INSERT INTO `tbl_product` (`p_id`, `t_id`, `p_name`, `p_detial`, `p_price`, `p_unit`, `p_img1`, `p_img2`, `p_img3`, `p_img4`, `p_img5`, `p_view`, `p_qty`, `p_size`, `p_ems`, `promo`, `date_save`) VALUES
-(43, 15, 'หมวกกันน็อคหนัง Biker Z1 clear', '<p><strong>หมวกกันน็อคหนัง Biker Z1 clear</strong><br />\r\nหมวกหนังดำ หนาอย่างดี ปลอดภัย หุ้มอย่างดี<br />\r\nสินค้าพรีเมียม โครตตตต เท่ !!!! หมวกหนังมีใบเดียวเท่านั้น!!!</p>\r\n\r\n<p><strong>หมวกกันน็อคหนัง Biker Z1 clear</strong><br />\r\nใครโชคดีจองเร็ว จัดไปครับ ดีไซน์โครตหล่อ<br />\r\n<strong>แถม หน้ากาก Biker Z1 และ แว่นกันลม</strong></p>\r\n\r\n<p><strong>ราคาพิเศษ 15,900บาท&nbsp; จากปกติ 21,000บ ของใหม่เอี่ยม มือหนึ่ง !!&nbsp;</strong><br />\r\nสินค้าพรีเมียม ของดี มีจำนวนจำกัด<br />\r\nโปรโมชั่นราคาพิเศษ ส่งฟรี&nbsp;ติดต่อมาได้เลยครับใบสุดท้าย</p>\r\n', 15900.43, 'ใบ', 'img1143426510720180828_220416.jpg', '', '', '', '', 10, 0, 's', 45, 32100.00, '2018-08-28 15:04:16'),
-(44, 15, 'หมวกหนังวัวแท้ Quinn Hat Coffee ', '<p><strong>หมวกหนังวัวแท้ Quinn Hat Coffee size XXL</strong><br />\r\nหล่อ ปัง มีสไตล์ กันฝน ท้าลม สู้แดด แมนได้อีกกก !!<br />\r\nดีไซต์เนี๊ยบ เท่ ระดับไฮคลาส</p>\r\n\r\n<p><strong>หมวกหนังวัวแท้ Quinn Hat Coffee size XXL</strong><br />\r\nเหมาะกับคุณผู้ชาย ชอบสไตล์คันทรี ที่อบอุ่น มีความชิว<br />\r\nดูดีขั้นเทพ สะดุดตา จนสาวๆ ต้องหลงรัก<br />\r\nพรีเมี่ยมขนาดนี้ ถ้าพลาดคงเสียดายสุดๆ !!!!!!</p>\r\n\r\n<p>โปรโมชั่นภายในเดือนนี้เท่านั้น!!<br />\r\n<strong>หมวกหนังวัวแท้ Quinn Hat Coffee size XXL</strong><br />\r\n<strong>ราคาพิเศษ 2,690 บาท จากราคาปกติ 4,500 บาท</strong><br />\r\nส่งฟรีแบบด่วน&nbsp;ติดต่อมาได้เลยครับใบสุดท้าย</p>\r\n', 2690.00, 'ใบ', 'img173858041320180828_220557.jpg', 'img273858041320180828_220557.jpg', '', '', '', 6, 12, 's', 45, 0.00, '2018-08-28 15:05:57'),
+(43, 15, 'หมวกกันน็อคหนัง Biker Z1 clear', '<p><strong>หมวกกันน็อคหนัง Biker Z1 clear</strong><br />\r\nหมวกหนังดำ หนาอย่างดี ปลอดภัย หุ้มอย่างดี<br />\r\nสินค้าพรีเมียม โครตตตต เท่ !!!! หมวกหนังมีใบเดียวเท่านั้น!!!</p>\r\n\r\n<p><strong>หมวกกันน็อคหนัง Biker Z1 clear</strong><br />\r\nใครโชคดีจองเร็ว จัดไปครับ ดีไซน์โครตหล่อ<br />\r\n<strong>แถม หน้ากาก Biker Z1 และ แว่นกันลม</strong></p>\r\n\r\n<p><strong>ราคาพิเศษ 15,900บาท&nbsp; จากปกติ 21,000บ ของใหม่เอี่ยม มือหนึ่ง !!&nbsp;</strong><br />\r\nสินค้าพรีเมียม ของดี มีจำนวนจำกัด<br />\r\nโปรโมชั่นราคาพิเศษ ส่งฟรี&nbsp;ติดต่อมาได้เลยครับใบสุดท้าย</p>\r\n', 15900.43, 'ใบ', 'img1143426510720180828_220416.jpg', '', '', '', '', 11, 0, 's', 45, 32100.00, '2018-08-28 15:04:16'),
+(44, 15, 'หมวกหนังวัวแท้ Quinn Hat Coffee ', '<p><strong>หมวกหนังวัวแท้ Quinn Hat Coffee size XXL</strong><br />\r\nหล่อ ปัง มีสไตล์ กันฝน ท้าลม สู้แดด แมนได้อีกกก !!<br />\r\nดีไซต์เนี๊ยบ เท่ ระดับไฮคลาส</p>\r\n\r\n<p><strong>หมวกหนังวัวแท้ Quinn Hat Coffee size XXL</strong><br />\r\nเหมาะกับคุณผู้ชาย ชอบสไตล์คันทรี ที่อบอุ่น มีความชิว<br />\r\nดูดีขั้นเทพ สะดุดตา จนสาวๆ ต้องหลงรัก<br />\r\nพรีเมี่ยมขนาดนี้ ถ้าพลาดคงเสียดายสุดๆ !!!!!!</p>\r\n\r\n<p>โปรโมชั่นภายในเดือนนี้เท่านั้น!!<br />\r\n<strong>หมวกหนังวัวแท้ Quinn Hat Coffee size XXL</strong><br />\r\n<strong>ราคาพิเศษ 2,690 บาท จากราคาปกติ 4,500 บาท</strong><br />\r\nส่งฟรีแบบด่วน&nbsp;ติดต่อมาได้เลยครับใบสุดท้าย</p>\r\n', 2690.00, 'ใบ', 'img173858041320180828_220557.jpg', 'img273858041320180828_220557.jpg', '', '', '', 6, 11, 's', 45, 0.00, '2018-08-28 15:05:57'),
 (45, 16, 'กระเป๋าใส่พวงกุญแจหนัง Gaston Blue', '<p><strong>กระเป๋าใส่พวงกุญแจหนัง Gaston Blue</strong><br />\r\nทำจาหนังแท้เกรดพรีเมี่ยม<br />\r\nใส่กุญแจรถ กุญแจบ้าน กุญแจเซฟ หรือแล้วแต่ต้องการ</p>\r\n\r\n<p>โปรโมชั่น<br />\r\n<strong>กระเป๋าใส่พวงกุญแจหนัง Gaston Blue</strong><br />\r\n<strong>ราคาพิเศษ 1,590บาท&nbsp; จากปกติ 3,400 บาท</strong><br />\r\nสินค้าพรีเมียม ของดี มีจำนวนจำกัด<br />\r\nโปรโมชั่นราคาพิเศษ ส่งฟรี&nbsp;ติดต่อมาได้เลยครับใบสุดท้าย</p>\r\n', 1590.00, 'ชิ้น', 'img1187125003120180828_220729.jpg', 'img2187125003120180828_220729.jpg', '', '', '', 4, 3, 's', 45, 3500.00, '2018-08-28 15:07:29'),
 (46, 16, 'สายกระเป๋าหนัง Strap', '<p><strong>สายกระเป๋าหนัง Strap</strong><br />\r\nสายกระเป๋าหนัง แข็งแรงสุด ๆ</p>\r\n', 450.00, 'ชิ้น', 'img1122020876020180828_220919.jpg', '', '', '', '', 6, 21, 's', 45, 0.00, '2018-08-28 15:09:19'),
-(47, 14, 'รองเท้าหนังแท้ Chilling Loafer Brown', '<p><strong>รองเท้าหนังแท้ Chilling Loafer Brown&nbsp;size40</strong><br />\r\nรองเท้าหนังสุดเท่ มี 3 สี Classic, Brown, Gray พร้อมส่ง ไซส์ 40 &ndash; 47<br />\r\nรองเท้าลำลอง ใส่ทำงานก็ได้ ใส่เที่ยวก็ดูดี<br />\r\nของดี คุณภาพคุณผู้ชายต้องชอบแน่นอน การันตี!!!</p>\r\n\r\n<p>&nbsp;</p>\r\n\r\n<p><strong>รองเท้าหนังแท้ Chilling Loafer Brown&nbsp;size40</strong><br />\r\nดีไซน์ออกแบบมาเป็นพิเศษ หล่อ เท่ห์ ไม่ซ้ำใคร<br />\r\nทำจากหนังวัวแท้&nbsp;ใส่นิ่ม สบายเท้า<br />\r\nพื้นรองเท้าทำจากยางคุณภาพดีที่สุด<br />\r\nระบายอากาศได้เป็นอย่างดี<br />\r\nกันน้ำ กันลม กันฝุ่น กันแดด กันได้ทุกอย่าง สุดยอด !!</p>\r\n\r\n<p><strong>รองเท้าหนังแท้ Chilling Loafer Brown&nbsp;size40</strong><br />\r\nทางร้านจัดโปรโมชั่นเฉพาะ 40 ท่านแรกเท่านั้น!!<br />\r\n<strong>ราคาพิเศษเพียง 4,250 บาท จากราคาปกติ 6,500 บาท</strong><br />\r\nพิเศษยิ่งกว่านั้น ..เราแถม&nbsp;ส่งฟรี EMS ทุกชิ้น!!</p>\r\n', 4250.00, 'คู่', 'img11133146420180828_221240.jpg', 'img21133146420180828_221240.jpg', '', '', '', 8, 20, 's', 45, 6200.00, '2018-08-28 15:12:40'),
-(40, 12, 'แจ็คเก็ตหนังPU สำหรับ Biker', '<p><strong>จ็คเก็ตหนังPU สำหรับ Biker</strong><br />\r\nทำจากหนังวัวผสม PU คุณภาพดี มีคุณสมบัติกันแดด กันฝน กันลม ดีเยี่ยม<br />\r\nเหมาะสำหรับ Biker</p>\r\n\r\n<p><strong>แจ็คเก็ตหนังPU สำหรับ Biker</strong><br />\r\nมี Size M L XL XXL XXXL</p>\r\n\r\n<p>&nbsp;</p>\r\n\r\n<p><strong>โปรโมชั่นราคาพิเศษเพียง 5,490 บาท จากราคาปกติ 8,500บาท</strong><br />\r\nพิเศษยิ่งกว่านั้น ..เราแถม&nbsp;ส่งฟรี EMS ทุกชิ้น!!</p>\r\n', 5490.00, 'ตัว', 'img1137514345820180828_215842.jpg', 'img229173571320180828_214536.jpg', '', '', '', 0, 12, 's', 45, 0.00, '2018-08-28 14:45:36'),
+(47, 14, 'รองเท้าหนังแท้ Chilling Loafer Brown', '<p><strong>รองเท้าหนังแท้ Chilling Loafer Brown&nbsp;size40</strong><br />\r\nรองเท้าหนังสุดเท่ มี 3 สี Classic, Brown, Gray พร้อมส่ง ไซส์ 40 &ndash; 47<br />\r\nรองเท้าลำลอง ใส่ทำงานก็ได้ ใส่เที่ยวก็ดูดี<br />\r\nของดี คุณภาพคุณผู้ชายต้องชอบแน่นอน การันตี!!!</p>\r\n\r\n<p>&nbsp;</p>\r\n\r\n<p><strong>รองเท้าหนังแท้ Chilling Loafer Brown&nbsp;size40</strong><br />\r\nดีไซน์ออกแบบมาเป็นพิเศษ หล่อ เท่ห์ ไม่ซ้ำใคร<br />\r\nทำจากหนังวัวแท้&nbsp;ใส่นิ่ม สบายเท้า<br />\r\nพื้นรองเท้าทำจากยางคุณภาพดีที่สุด<br />\r\nระบายอากาศได้เป็นอย่างดี<br />\r\nกันน้ำ กันลม กันฝุ่น กันแดด กันได้ทุกอย่าง สุดยอด !!</p>\r\n\r\n<p><strong>รองเท้าหนังแท้ Chilling Loafer Brown&nbsp;size40</strong><br />\r\nทางร้านจัดโปรโมชั่นเฉพาะ 40 ท่านแรกเท่านั้น!!<br />\r\n<strong>ราคาพิเศษเพียง 4,250 บาท จากราคาปกติ 6,500 บาท</strong><br />\r\nพิเศษยิ่งกว่านั้น ..เราแถม&nbsp;ส่งฟรี EMS ทุกชิ้น!!</p>\r\n', 4250.00, 'คู่', 'img11133146420180828_221240.jpg', 'img21133146420180828_221240.jpg', '', '', '', 8, 18, 's', 45, 6200.00, '2018-08-28 15:12:40'),
+(40, 12, 'แจ็คเก็ตหนังPU สำหรับ Biker', '<p><strong>จ็คเก็ตหนังPU สำหรับ Biker</strong><br />\r\nทำจากหนังวัวผสม PU คุณภาพดี มีคุณสมบัติกันแดด กันฝน กันลม ดีเยี่ยม<br />\r\nเหมาะสำหรับ Biker</p>\r\n\r\n<p><strong>แจ็คเก็ตหนังPU สำหรับ Biker</strong><br />\r\nมี Size M L XL XXL XXXL</p>\r\n\r\n<p>&nbsp;</p>\r\n\r\n<p><strong>โปรโมชั่นราคาพิเศษเพียง 5,490 บาท จากราคาปกติ 8,500บาท</strong><br />\r\nพิเศษยิ่งกว่านั้น ..เราแถม&nbsp;ส่งฟรี EMS ทุกชิ้น!!</p>\r\n', 5490.00, 'ตัว', 'img1137514345820180828_215842.jpg', 'img229173571320180828_214536.jpg', '', '', '', 0, 11, 's', 45, 0.00, '2018-08-28 14:45:36'),
 (41, 13, 'เข็มขัดหนัง Smart Man แบบ1', '<p><strong>เข็มขัดหนัง Smart Man หัวเข็มขัดแบบ1</strong><br />\r\nหัวเข็มขัดถอดเปลี่ยนได้ !!!<br />\r\nหัวเข็มขัดแสตนเลสอย่างดี โครตเท่<br />\r\nเนื้อดี หนังวัวแท้ นุ่ม หนา ทนทาน ใช้ได้ยันลูกหลาน<br />\r\nของดี คุณภาพ หนา คุณผู้ชายต้องชอบแน่นอน การีนตี !!!<br />\r\nขนาดความยาว 120 cm กว้าง 3.80 cm</p>\r\n\r\n<p>ทางร้านจัดโปรโมชั่น ภายในเดือนนี้เท่านั้น !!!<br />\r\n<strong>เข็มขัดหนัง Smart Man หัวเข็มขัดแบบ1</strong><br />\r\n40 ท่านแรกเท่านั้น<br />\r\n<strong>ราคาพิเศษ 1,690 บาท !! จากราคาปกติ 3,500 บาท</strong><br />\r\nพิเศษยิ่งกว่านั้น ..เราแถม&nbsp;ส่งฟรี EMS ทุกชิ้น!!</p>\r\n', 1690.00, 'ชิ้น', 'img1118127783720180828_215716.jpg', 'img268731187420180828_215002.jpg', '', '', '', 1, 54, 's', 45, 0.00, '2018-08-28 14:50:02'),
-(42, 14, 'รองเท้าบูท Cowboy Boots size44', '<p><strong>รองเท้าบูท Cowboy Boots size44</strong><br />\r\nทำจากหนังวัวแท้ ระดับพรีเมี่ยม<br />\r\nเท่ มีสไตลย์ จะลุย เที่ยว ทำงาน ปาร์ตี้ ก็ได้<br />\r\nกันน้ำ กันลม กันฝุ่น กันแดด<br />\r\nของดี คุณภาพคุณผู้ชายต้องชอบแน่นอน</p>\r\n\r\n<p>&nbsp;</p>\r\n\r\n<p><strong>รองเท้าบูท Cowboy Boots size44</strong><br />\r\nดีไซน์ออกแบบมาเป็นพิเศษ หล่อ เท่ห์ ไม่ซ้ำใคร<br />\r\nทำจากหนังวัวแท้&nbsp;ใส่นิ่ม สบายเท้า<br />\r\nพื้นรองเท้าทำจากยางคุณภาพดีที่สุด</p>\r\n\r\n<p><strong>รองเท้าบูท Cowboy Boots size44</strong><br />\r\nทางร้านจัดโปรโมชั่น<br />\r\n<strong>ราคาพิเศษเพียง 8,990 บาท จากราคาปกติ 13,500 บาท</strong><br />\r\nพิเศษยิ่งกว่านั้น ..เราแถม&nbsp;ส่งฟรี EMS ทุกชิ้น!!</p>\r\n', 8999.00, 'คู่', 'img156718228120180828_215622.jpg', 'img2110152471120180828_215228.jpg', '', '', '', 2, 12, 's', 45, 0.00, '2018-08-28 14:52:28'),
-(37, 10, 'กระเป๋าสตางค์ Wallet Easy-S Black', '<p><strong>กระเป๋าสตางค์ Wallet Easy-S Black</strong>&nbsp;กระเป๋าหนังแท้<br />\r\nขนาดกำลังพอเหมาะ มีช่องใส่ธนบัตร ใส่สตางค์<br />\r\nสามารถคล้องกุญแจได้ เหมาะสำหรับพกพาไปทุกที่</p>\r\n\r\n<p><strong>กระเป๋าสตางค์ Wallet Easy-S Black</strong><br />\r\n<strong>โปรโมชั่น ราคาพิเศษ 1,690 บาท จากปกติ 1,990 บาท</strong><br />\r\nส่ง EMS ฟรี</p>\r\n', 1690.00, 'ใบ', 'img1112233121020180828_220133.jpg', 'img2201620226920180828_213944.jpg', '', '', '', 0, 12, 's', 45, 0.00, '2018-08-28 14:39:44'),
+(42, 14, 'รองเท้าบูท Cowboy Boots size44', '<p><strong>รองเท้าบูท Cowboy Boots size44</strong><br />\r\nทำจากหนังวัวแท้ ระดับพรีเมี่ยม<br />\r\nเท่ มีสไตลย์ จะลุย เที่ยว ทำงาน ปาร์ตี้ ก็ได้<br />\r\nกันน้ำ กันลม กันฝุ่น กันแดด<br />\r\nของดี คุณภาพคุณผู้ชายต้องชอบแน่นอน</p>\r\n\r\n<p>&nbsp;</p>\r\n\r\n<p><strong>รองเท้าบูท Cowboy Boots size44</strong><br />\r\nดีไซน์ออกแบบมาเป็นพิเศษ หล่อ เท่ห์ ไม่ซ้ำใคร<br />\r\nทำจากหนังวัวแท้&nbsp;ใส่นิ่ม สบายเท้า<br />\r\nพื้นรองเท้าทำจากยางคุณภาพดีที่สุด</p>\r\n\r\n<p><strong>รองเท้าบูท Cowboy Boots size44</strong><br />\r\nทางร้านจัดโปรโมชั่น<br />\r\n<strong>ราคาพิเศษเพียง 8,990 บาท จากราคาปกติ 13,500 บาท</strong><br />\r\nพิเศษยิ่งกว่านั้น ..เราแถม&nbsp;ส่งฟรี EMS ทุกชิ้น!!</p>\r\n', 8999.00, 'คู่', 'img156718228120180828_215622.jpg', 'img2110152471120180828_215228.jpg', '', '', '', 2, 11, 's', 45, 0.00, '2018-08-28 14:52:28'),
+(37, 10, 'กระเป๋าสตางค์ Wallet Easy-S Black', '<p><strong>กระเป๋าสตางค์ Wallet Easy-S Black</strong>&nbsp;กระเป๋าหนังแท้<br />\r\nขนาดกำลังพอเหมาะ มีช่องใส่ธนบัตร ใส่สตางค์<br />\r\nสามารถคล้องกุญแจได้ เหมาะสำหรับพกพาไปทุกที่</p>\r\n\r\n<p><strong>กระเป๋าสตางค์ Wallet Easy-S Black</strong><br />\r\n<strong>โปรโมชั่น ราคาพิเศษ 1,690 บาท จากปกติ 1,990 บาท</strong><br />\r\nส่ง EMS ฟรี</p>\r\n', 1690.00, 'ใบ', 'img1112233121020180828_220133.jpg', 'img2201620226920180828_213944.jpg', '', '', '', 0, 11, 's', 45, 0.00, '2018-08-28 14:39:44'),
 (38, 11, 'กระเป๋าเดินทาง หนังวัว Jarvoz Migana', '<p><strong>กระเป๋าเดินทาง หนังวัว Jarvoz Migana</strong><br />\r\nเหมาะสำหรับผู้ที่ชอบแบบง่ายๆ ถือก็ได้ สะพายเป็นเป้ก็ดี<br />\r\nรับรองว่า ติดใจ ทั้งคนใช้ และคนมอง<br />\r\nใครเห็นก็อยากได้ ใครใช้ก็ดูดี<br />\r\nกระเป๋า 2 in 1 เพราะมีช่องใส่ของหลายช่อง เลือกได้ตามใจชอบ<br />\r\nจุของได้เยอะ สะใจ หนังแท้ หนังหนา เคลือบแว๊กพิเศษ</p>\r\n\r\n<p><strong>กระเป๋าเดินทาง หนังวัว Jarvoz Migana</strong><br />\r\n<strong>จัดโปรโมชั่น ราคาพิเศษ 10,900 บาท จากปกติ 19,990 บาท</strong></p>\r\n', 10900.00, 'ใบ', 'img127119811920180828_215958.jpg', 'img2192568450420180828_214137.jpg', '', '', '', 1, 12, 's', 45, 0.00, '2018-08-28 14:41:37'),
 (39, 11, 'กระเป๋าเป้ Rocket Bag', '<p><strong>กระเป๋าเป้ Rocket Bag</strong><br />\r\nตามเทรน Pokemon Go ด้วยกระเป๋าเป้หนังแท้<br />\r\n<strong>Genuine First Layer Cow Genuine First Layer Cow Leather</strong><br />\r\n<strong>รุ่น Rocket Bag</strong>&nbsp;เกรด พรีเมี่ยมมมม โอ้โหวววว สุดยอด<br />\r\nสำผัสนุ่ม น่าหลงไหล มีไว้รับรองคุ้มมมม<br />\r\nการรันดี หนังคุณภาพดี 100%<br />\r\nสี Chocolate คลาสสิคสุดๆ<br />\r\nน้ำหนัก 1.75 Kg เท่านั้น เหมาะสำหรับพกพา ไปตามล่า Pokemon<br />\r\nกันเลยคร้าบบบ !!!</p>\r\n\r\n<p>โปรโมชั่นภายในเดือนนี้เท่านั้น!!<br />\r\n<strong>กระเป๋า JARVOZ รุ่น Rocket Bag</strong>&nbsp;ราคาพิเศษ 18,900 บาท<br />\r\nพิเศษยิ่งกว่านั้น ..เราแถม&nbsp;ส่งฟรีแบบด่วนเฉพาะ 40 ท่านแรกเท่านั้นคร้าบบ</p>\r\n', 18900.00, 'ใบ', 'img1113102335920180828_215919.jpg', 'img2209658065920180828_214354.jpg', '', '', '', 0, 12, 's', 45, 0.00, '2018-08-28 14:43:54'),
 (36, 10, 'กระเป๋าสตางค์ Long Wallet-Pumar', '<p><strong>กระเป๋าสตางค์ Long Wallet-Pumar&nbsp;</strong>กระเป๋าหนังวัวแท้ เกรดพรีเมี่ยม<br />\r\nขนาดกำลังพอเหมาะ มีช่องใส่ธนบัตร ใส่สตางค์ ใส่ IPhone บัตรต่างๆ</p>\r\n\r\n<p><strong>กระเป๋าสตางค์ Long Wallet-Pumar</strong><br />\r\nจัดโปรโมชั่นสมนาคุณลูกค้า<br />\r\n<strong>ราคาพิเศษ 2,190 บาท จากปกติ 4,290 บาท</strong><br />\r\nส่ง EMSฟรี</p>\r\n', 2190.00, 'ใบ', 'img13709343920180828_220207.jpg', 'img2144583376820180828_213658.jpg', '', '', '', 0, 32, 's', 45, 0.00, '2018-08-28 14:36:58'),
@@ -264,6 +265,12 @@ ALTER TABLE `tbl_bank`
   ADD PRIMARY KEY (`b_id`);
 
 --
+-- Indexes for table `tbl_log`
+--
+ALTER TABLE `tbl_log`
+  ADD PRIMARY KEY (`log_id`);
+
+--
 -- Indexes for table `tbl_member`
 --
 ALTER TABLE `tbl_member`
@@ -301,13 +308,19 @@ ALTER TABLE `tbl_type`
 -- AUTO_INCREMENT for table `tbl_admin`
 --
 ALTER TABLE `tbl_admin`
-  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `tbl_bank`
 --
 ALTER TABLE `tbl_bank`
   MODIFY `b_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `tbl_log`
+--
+ALTER TABLE `tbl_log`
+  MODIFY `log_id` int(10) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `tbl_member`
@@ -319,13 +332,13 @@ ALTER TABLE `tbl_member`
 -- AUTO_INCREMENT for table `tbl_order`
 --
 ALTER TABLE `tbl_order`
-  MODIFY `order_id` int(6) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `order_id` int(6) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `tbl_order_detail`
 --
 ALTER TABLE `tbl_order_detail`
-  MODIFY `d_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `d_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `tbl_product`
