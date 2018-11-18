868948a7eedea6e4ee41ea567f7a0f6bfba8cd7a
nopparat231 <23.noop@gmail.com>
2018-11-18 14:49:20 +0700

    up utf-8

--- register_db.php
+++ register_db.php
@@ -47,7 +47,7 @@ $result1 = mysql_query($sql,$condb) or die ("Error in query : $sql" .mysql_error
 
 		$strTo = $mem_email;
 		$strSubject = "Activate Member Account";
-		$strHeader = "Content-type: text/html; charset=windows-874\n"; // or UTF-8 //
+		$strHeader = "Content-type: text/html; charset=UTF-8\n"; // or UTF-8 //
 		$strHeader .= "From: webmaster@thaicreate.com\nReply-To: webmaster@thaicreate.com";
 		$strMessage = "";
 		$strMessage .= "Welcome : ".$mem_name."<br>";
